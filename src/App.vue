1ea7c3250f01171ee35113107b53e5124b1f762e
Artem <der1302@mail.ru>
2021-09-07 08:45:26 +0300

    first commit
    lay the foundation of project

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="wrapper">
+      <div class="img-container">
+          <img src="" alt="">
+      </div>
+      <div class="tickets-block">
+          <div class="tickets-block__sidebar"></div>
+          <div class="tickets-block__result-info"></div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data() {
+        return {
+            text: 'Hello World!'
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+$radius: 5px;
+
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+img {
+    max-width: 100%;
+    height: auto;
+}
+
+html {
+    background: #f3f7fa;
+    font-size: 16px;
+}
+.wrapper {
+    max-width: 1180px;
+    padding: 0 15px;
+    .img-container {
+        max-width: 400px;
+        text-align: center;
+    }
+    .tickets-block {
+        display: flex;
+        flex-wrap: wrap;
+        &__sidebar {
+            flex: 0 0 25%;
+            max-width: 25%;
+        }
+        &__result-info {
+            flex: 0 0 75%;
+            max-width: 75%;
+        }
+    }
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const vueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    entry: './index.js',
+    devtool: 'inline-source-map',
+    mode: 'production',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ],
+            }
+        ]
+    },
+    plugins: [
+        new vueLoaderPlugin()
+    ],
+    devServer: {
+        static: './dist'
+    }
+}
